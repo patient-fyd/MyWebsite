463a6ef413cca887373eb59792d7ef573808e49b
gfy <patient.fyd@gmail.com>
2024-10-18 21:12:18 +0800

    给页脚添加备案号

--- blog-frontend/src/components/common/Footer.vue
+++ blog-frontend/src/components/common/Footer.vue
@@ -1,9 +1,17 @@
 <template>
   <el-footer class="footer">
     <div class="footer-content">
+      <!-- 左侧：版权信息 -->
       <div class="copyright">
         &copy; 2024 YourWebsite. All rights reserved.
       </div>
+
+      <!-- 中间：备案号 -->
+      <div class="record-number">
+        <a href="https://beian.miit.gov.cn/" target="_blank">赣ICP备2024046184号-1</a>
+      </div>
+
+      <!-- 右侧：社交媒体链接 -->
       <div class="social-links">
         <el-icon @click="openLink('https://facebook.com')">
           <i class="fab fa-facebook"></i>
@@ -38,6 +46,12 @@ const openLink = (url) => {
   align-items: center;
 }
 
+.record-number {
+  flex: 1;
+  text-align: center;
+  font-size: 14px;
+}
+
 .social-links {
   display: flex;
   gap: 10px;
@@ -50,4 +64,3 @@ const openLink = (url) => {
 </style>
 
 <!-- 在index.html中引入Font Awesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
