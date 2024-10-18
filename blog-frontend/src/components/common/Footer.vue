e626c581a397380479e3b1b608dea7b50e0a59c1
gfy <patient.fyd@gmail.com>
2024-10-18 20:40:09 +0800

    添加页脚组件

---
+++ blog-frontend/src/components/common/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-footer class="footer">
+    <div class="footer-content">
+      <div class="copyright">
+        &copy; 2024 YourWebsite. All rights reserved.
+      </div>
+      <div class="social-links">
+        <el-icon @click="openLink('https://facebook.com')">
+          <i class="fab fa-facebook"></i>
+        </el-icon>
+        <el-icon @click="openLink('https://twitter.com')">
+          <i class="fab fa-twitter"></i>
+        </el-icon>
+        <el-icon @click="openLink('https://linkedin.com')">
+          <i class="fab fa-linkedin"></i>
+        </el-icon>
+      </div>
+    </div>
+  </el-footer>
+</template>
+
+<script setup>
+const openLink = (url) => {
+  window.open(url, '_blank');
+};
+</script>
+
+<style scoped>
+.footer {
+  padding: 20px;
+  background-color: #f8f9fa;
+  text-align: center;
+}
+
+.footer-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.social-links {
+  display: flex;
+  gap: 10px;
+}
+
+.social-links i {
+  font-size: 24px;
+  cursor: pointer;
+}
+</style>
+
+<!-- 在index.html中引入Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
