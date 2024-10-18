f9d66978b5097ae23a884bc1a7571b2527564961
Gan FengYu <patient.fyd@gmail.com>
2024-10-18 08:18:06 +0800

    添加软删除字段

--- blog-backend/models/post.go
+++ blog-backend/models/post.go
@@ -1,18 +1,22 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Post struct {
-	ID         uint      `gorm:"primaryKey" json:"id"`
-	Title      string    `json:"title"`
-	Content    string    `json:"content"`
-	CategoryID uint      `json:"category_id"`
-	Category   Category  `gorm:"foreignKey:CategoryID" json:"category"`
-	AuthorID   uint      `json:"author_id"`
-	Author     User      `gorm:"foreignKey:AuthorID" json:"author"`
-	Tags       []Tag     `gorm:"many2many:post_tags;" json:"tags"`
-	Comments   []Comment `gorm:"foreignKey:PostID" json:"comments"`
-	Views      uint      `json:"views"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	ID         uint           `gorm:"primaryKey" json:"id"`
+	Title      string         `json:"title"`
+	Content    string         `json:"content"`
+	CategoryID uint           `json:"category_id"`
+	Category   Category       `gorm:"foreignKey:CategoryID" json:"category"`
+	AuthorID   uint           `json:"author_id"`
+	Author     User           `gorm:"foreignKey:AuthorID" json:"author"`
+	Tags       []Tag          `gorm:"many2many:post_tags;" json:"tags"`
+	Comments   []Comment      `gorm:"foreignKey:PostID" json:"comments"`
+	Views      uint           `json:"views"`
+	CreatedAt  time.Time      `json:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at"`
+	DeletedAt  gorm.DeletedAt `gorm:"index" json:"-"` // 用于软删除
 }
