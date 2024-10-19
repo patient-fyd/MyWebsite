55afe12868e964447be3fa0318881703a3657990
gfy <patient.fyd@gmail.com>
2024-10-19 15:04:46 +0800

    修改跨域端口

--- blog-backend/middleware/cors.go
+++ blog-backend/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func CORSConfig() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"}, // 允许的前端域名
+		AllowOrigins:     []string{"http://localhost:5173"}, // 允许的前端域名
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
