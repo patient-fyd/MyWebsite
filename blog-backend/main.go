c9420da3b6fc5bb480123c39c186a190054261ad
gfy <patient.fyd@gmail.com>
2024-10-19 14:49:30 +0800

    添加跨域请求配置

--- blog-backend/main.go
+++ blog-backend/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/patient-fyd/blog-backend/config"
+	"github.com/patient-fyd/blog-backend/middleware"
 	"github.com/patient-fyd/blog-backend/routers"
 )
 
 func main() {
 	r := gin.Default()
-	config.Setup()         // 初始化配置
-	routers.SetupRoutes(r) // 设置路由
-	r.Run(":8080")         // 运行服务
+	config.Setup()                 // 初始化配置
+	r.Use(middleware.CORSConfig()) //跨域
+	routers.SetupRoutes(r)         // 设置路由
+	r.Run(":8080")                 // 运行服务
 }
---
+++ blog-backend/middleware/cors.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func CORSConfig() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // 允许的前端域名
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
