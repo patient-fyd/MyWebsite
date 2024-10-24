a11b9692f6b8bca7c13b79586e42ab16542f523f
gfy <patient.fyd@gmail.com>
2024-10-24 13:23:01 +0800

    微调界面，

--- blog-frontend/src/views/CreateArticle.vue
+++ blog-frontend/src/views/CreateArticle.vue
@@ -69,13 +69,23 @@ import { debounce } from "lodash";
 import { MdEditor } from "md-editor-v3";
 import "md-editor-v3/lib/style.css";
 import "highlight.js/styles/github.css";
+import mermaid from "mermaid";
 import SelectModal from "@/components/createArticle/SelectModal.vue";
 import Modal from "@/components/createArticle/Modal.vue";
 import { useArticleStore } from "@/stores/articleStore";
 import { QuillEditor } from "@vueup/vue-quill";
 
+console.log(
+  "不要吹灭你的灵感和你的想象力; 不要成为你的模型的奴隶。 ——文森特・梵高",
+);
+
+mermaid.initialize({
+  startOnLoad: true,
+  theme: "default",
+  logLevel: "fatal",
+});
 const title = ref("");
-const markdownContent = ref("# 在这里开始你的 Markdown 编辑");
+const markdownContent = ref("### 在这里开始你的 Markdown 编辑");
 const content = ref(""); // Quill 编辑器的内容
 const isUsingQuill = ref(false); // 初始设置为使用 MdEditor
 const showPublishModal = ref(false);
@@ -96,16 +106,18 @@ const editorOptions = ref({
   },
 });
 
+savingStatus.value = "文章将自动保存！";
 // 自动保存的函数
 const saveContent = async () => {
   savingStatus.value = "保存中...";
   try {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     savingStatus.value = "保存成功！";
-    setTimeout(() => (savingStatus.value = ""), 3000);
+    setTimeout(() => (savingStatus.value = "文章将自动保存"), 3000);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     savingStatus.value = "保存失败";
-    setTimeout(() => (savingStatus.value = ""), 3000);
+    setTimeout(() => (savingStatus.value = "请手动保存"), 3000);
   }
 };
 
@@ -120,7 +132,7 @@ const publishArticle = async () => {
     await articleStore.createArticle(title.value, markdownContent.value);
     alert("文章发布成功！");
     title.value = "";
-    markdownContent.value = "# 在这里开始你的 Markdown 编辑";
+    markdownContent.value = "### 在这里开始你的 Markdown 编辑";
     showPublishModal.value = false;
   } catch (error) {
     console.error("发布文章失败", error);
@@ -171,7 +183,6 @@ body {
   flex-direction: column;
   height: 100vh; /* 高度设为视口高度 */
   width: 100vw; /* 宽度设为视口宽度 */
-  padding: 20px;
   box-sizing: border-box;
 }
 
@@ -180,11 +191,13 @@ body {
   justify-content: space-between;
   align-items: center;
   margin-bottom: 20px;
+  margin-top: 10px;
 }
 
 .input-group {
   display: flex;
   align-items: center;
+  margin-left: 10px;
   flex-grow: 1; /* 确保 input-group 可以填满除按钮组外的空间 */
 }
 
@@ -196,8 +209,8 @@ body {
 }
 
 .save-status {
-  margin-left: 10px; /* 保存状态信息与输入框的距离 */
-  color: #555; /* 保存状态文字颜色 */
+  margin-left: 15px; /* 保存状态信息与输入框的距离 */
+  color: #a89f9f; /* 保存状态文字颜色 */
   font-size: 0.9em;
   white-space: nowrap; /* 防止文字换行 */
 }
@@ -219,6 +232,10 @@ body {
   display: inline-flex; /* 保持和按钮一样的布局 */
   align-items: center;
   justify-content: center;
+}
+
+.button-link {
+  margin-right: 10px;
 }
 
 .button-group button:hover,
