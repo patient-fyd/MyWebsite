204a798a1adb724b58b0acb7d1e867e07a5c8106
gfy <patient.fyd@gmail.com>
2024-12-08 14:22:29 +0800

    Update ArticleList.vue

--- blog-frontend/src/components/article/ArticleList.vue
+++ blog-frontend/src/components/article/ArticleList.vue
@@ -25,10 +25,9 @@
       <div class="article-footer">
         <p>
           标签:
-          <span v-for="(tag, tagIndex) in article.tags || []" :key="tagIndex"
-            >{{ tag
-            }}{{ tagIndex < (article.tags || []).length - 1 ? ", " : "" }}</span
-          >
+          <span v-for="(tag, tagIndex) in article.tags" :key="tagIndex">
+            {{ tag.name }}{{ tagIndex < article.tags.length - 1 ? ", " : "" }}
+          </span>
         </p>
         <button @click="readMore(article.id)">阅读更多</button>
       </div>
